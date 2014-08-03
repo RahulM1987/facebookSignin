4e5d2dbb398c0312c65113814eff05a21669ac83
Rahul Morade <rahul@maxdigi.com>
2014-08-03 10:51:39 +0530

    Initial Commit

---
+++ facebookSignin/MainAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MainAppDelegate.h
+//  facebookSignin
+//
+//  Created by Rahul on 03/08/14.
+//  Copyright (c) 2014 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ facebookSignin/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  facebookSignin
+//
+//  Created by Rahul on 03/08/14.
+//  Copyright (c) 2014 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MainAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MainAppDelegate class]));
+    }
+}
